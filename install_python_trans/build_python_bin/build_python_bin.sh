77acb9fb0a24bd6828d7f61df9cc2fba6f144220
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2025-05-23 18:49:51 +0100

    Update build_python_bin.sh

--- install_python_trans/build_python_bin/build_python_bin.sh
+++ install_python_trans/build_python_bin/build_python_bin.sh
@@ -3,6 +3,6 @@ cd $TRANS_VENV
 # your Python version here
 PYTHON_VERSION=python3.10
 $TRANS_VENV/bin/pip install pyinstaller
-$TRANS_VENV/bin/pyinstaller --onefile --python-option u  --recursive-copy-metadata "openai-whisper" --add-data "$TRANS_VENV/lib/$PYTHON_VERSION/site-packages/whisper:whisper/" --add-data "$TRANS_VENV/lib/$PYTHON_VERSION/site-packages/transcribe_translate:transcribe_and_translate/" --console $TRANS_VENV/lib/$PYTHON_VERSION/site-packages/transcribe_translate/transcribe_and_translate_cli.py
+$TRANS_VENV/bin/pyinstaller --python-option u  --recursive-copy-metadata "openai-whisper" --add-data "$TRANS_VENV/lib/$PYTHON_VERSION/site-packages/whisper:whisper/" --add-data "$TRANS_VENV/lib/$PYTHON_VERSION/site-packages/transcribe_translate:transcribe_and_translate/" --console $TRANS_VENV/lib/$PYTHON_VERSION/site-packages/transcribe_translate/transcribe_and_translate_cli.py
 
 # optional: --onefile
